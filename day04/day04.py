9d3e941a7ce6ede28bea0a6d577e3b2f990a11d9
Guido Weber <guido.weber0815@googlemail.com>
2018-12-04 20:39:16 +0100

    Day 4 complete

--- day04/day04.py
+++ day04/day04.py
@@ -10,15 +10,18 @@ def read_lines():
 GUARD_PATTERN = re.compile(r'Guard #(\d+) begins shift')
 
 
-def main(lines):
+def parse_events(lines):
+    # Sorting the lines alphabetically is enough to bring them in the correct order
     lines.sort()
-    sleepcounts = defaultdict(lambda: Counter())
+    # The result of this function is a mapping (guard, minute) -> #asleep
+    result = defaultdict(Counter)
     guard = None
     awake = True
     went_to_sleep = None
+    # For each sleep phase, add 1 to each minute of that phase for the guard.
+    # Added additional checks to verify event ordering is plausible.
     for line in lines:
-        dt = line[1:17]
-        minute = int(dt[-2:])
+        minute = int(line[15:17])
         txt = line[19:]
         match = GUARD_PATTERN.match(txt)
         if awake and match is not None:
@@ -28,11 +31,13 @@ def main(lines):
             went_to_sleep = minute
         elif not awake and guard is not None and txt == 'wakes up':
             awake = True
-            for m in range(went_to_sleep, minute):
-                sleepcounts[guard][m] += 1
+            result[guard].update(range(went_to_sleep, minute))
         else:
-            raise RuntimeError("Oops: '%s'" % txt)
+            raise RuntimeError("Oops: %s" % line)
+    return result
 
+
+def part1(sleepcounts):
     max_sleep = 0
     for guard, counter in sleepcounts.items():
         sleepsum = sum(counter.values())
@@ -45,7 +50,25 @@ def main(lines):
     sleepiest = [m for m, cnt in sleepcounts[most_sleepy].items() if cnt == max_slept]
     print("Slept most (%d) in minute(s) %s" % (max_slept, ', '.join(('%d' % m) for m in sleepiest)))
 
-    print("Answer = %d" % (most_sleepy * sleepiest[0]))
+    print("Part 1 answer = %d" % (most_sleepy * sleepiest[0]))
+
+
+def part2(sleepcounts):
+    max_count = 0
+    for guard, counter in sleepcounts.items():
+        [(minute, cnt)] = counter.most_common(1)
+        if cnt > max_count:
+            the_guard = guard
+            the_minute = minute
+            max_count = cnt
+    print("Part 2: guard = %d, minute = %d, answer = %d"
+          % (the_guard, the_minute, the_guard * the_minute))
+
+
+def main(lines):
+    sleepcounts = parse_events(lines)
+    part1(sleepcounts)
+    part2(sleepcounts)
 
 
 if __name__ == '__main__':
