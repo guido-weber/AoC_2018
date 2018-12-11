1fcfc017e7fca47a5ead06a8b7f2dc90200c97e6
Guido Weber <guido.weber0815@googlemail.com>
2018-12-11 22:09:07 +0100

    Day 11 part 2 - still way too slow

--- day11.py
+++ day11.py
@@ -1,6 +1,3 @@
-from collections import Counter
-
-
 def power_level(x, y, grid_sn):
     rack_id = x + 10
     result = rack_id * y + grid_sn
@@ -9,28 +6,38 @@ def power_level(x, y, grid_sn):
 
 
 def create_grid(grid_sn):
-    return {(x, y): power_level(x, y, grid_sn)
-            for x in range(1, 301)
-            for y in range(1, 301)}
+    return [[power_level(x, y, grid_sn) for y in range(301)]
+            for x in range(301)]
 
 
-def total_power(grid, x, y, sq):
-    return sum(grid[(xx, yy)]
-               for xx in range(x, x + sq)
-               for yy in range(y, y + sq))
+def best(grid_sn, sq):
+    grid = create_grid(grid_sn)
+
+    def compute():
+        for x in range(1, 301 - sq):
+            for y in range(1, 301 - sq):
+                tp = sum(grid[xx][yy] for xx in range(x, x + sq) for yy in range(y, y + sq))
+                yield (x, y, tp)
+    return max(compute(), key=lambda tpl: tpl[2])
 
 
-def best(grid, sq):
-    c = Counter({(x, y): total_power(grid, x, y, sq)
-                 for x in range(1, 301 - sq)
-                 for y in range(1, 301 - sq)})
-    return c.most_common(1)[0]
-
-
-def all_best(grid):
-    i = ((sq, best(grid, sq)) for sq in range(1, 301))
-    c = Counter({(x, y, sq): pwr for (sq, ((x, y), pwr)) in i})
-    return c.most_common(1)[0]
+def all_best(grid_sn):
+    def compute():
+        grid = create_grid(grid_sn)
+        for x in range(1, 301):
+            for y in range(1, 301):
+                yield (x, y, 1, grid[x][y])
+        cur = create_grid(grid_sn)
+        for sq in range(2, 301):
+            for x in range(1, 301 - sq):
+                for y in range(1, 301 - sq):
+                    tp = (cur[x][y]
+                          + sum(grid[xx][y + sq - 1] for xx in range(x, x + sq - 1))
+                          + sum(grid[x + sq - 1][yy] for yy in range(y, y + sq - 1))
+                          + grid[x + sq - 1][y + sq - 1])
+                    cur[x][y] = tp
+                    yield (x, y, sq, tp)
+    return max(compute(), key=lambda tpl: tpl[3])
 
 
 if __name__ == '__main__':
@@ -38,16 +45,12 @@ if __name__ == '__main__':
     assert power_level(217, 196, 39) == 0
     assert power_level(101, 153, 71) == 4
 
-    assert best(create_grid(18), 3) == ((33, 45), 29)
-    assert best(create_grid(42), 3) == ((21, 61), 30)
-    ((x, y), pwr) = best(create_grid(5235), 3)
+    assert best(18, 3) == (33, 45, 29)
+    assert best(42, 3) == (21, 61, 30)
+    x, y, pwr = best(5235, 3)
     print("Part 1: %s at %d,%d" % (pwr, x, y))
 
-    print(all_best(create_grid(18)))
-    #assert best(create_grid(18), 3) == ((33, 45), 29)
-    #assert best(create_grid(18), 3) == ((33, 45), 29)
-
-"""
-For grid serial number 18, the largest total square (with a total power of 113) is 16x16 and has a top-left corner of 90,269, so its identifier is 90,269,16.
-For grid serial number 42, the largest total square (with a total power of 119) is 12x12 and has a top-left corner of 232,251, so its identifier is 232,251,12.
-"""
+    # assert all_best(18) == (90, 269, 16, 113)
+    # assert all_best(42) == (232, 251, 12, 119)
+    x, y, sq, pwr = all_best(5235)
+    print("Part 2: %s at %d,%d,%d" % (pwr, x, y, sq))
