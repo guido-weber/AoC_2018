2295eedfa952709a516396505075c769ce98c290
Guido Weber <guido.weber0815@googlemail.com>
2018-12-16 14:47:07 +0100

    Day 16 part 2

--- day16/day16.py
+++ day16/day16.py
@@ -11,7 +11,7 @@ P_BEFORE = re.compile(r'Before:.*(\d+),\s*(\d+),\s*(\d+),\s*(\d+).*')
 P_AFTER = re.compile(r'After:.*(\d+),\s*(\d+),\s*(\d+),\s*(\d+).*')
 
 
-def extract_samples(lines):
+def extract_input(lines):
     lnum = 0
     samples = []
     while lines[lnum].startswith('Before'):
@@ -21,23 +21,61 @@ def extract_samples(lines):
         samples.append((before, instruction, after))
         assert lines[lnum + 3] == ''
         lnum += 4
-    return samples
+    program = []
+    while lines[lnum] == '':
+        lnum += 1
+    for line in lines[lnum:]:
+        program.append([int(x) for x in line.split()])
+    return samples, program
 
 
-def count_possible_opcodes(sample):
-    result = 0
+def possible_opcodes(sample):
+    opcodes = set()
     for instruction in Instruction.ALL.values():
         i = instruction(*sample[1][1:])
         if i(sample[0]) == sample[2]:
-            result += 1
-    return result
+            opcodes.add(instruction.__name__)
+    return opcodes
+
+
+def find_opcodes(samples):
+    opcodes = {}
+    for sample in samples:
+        opcode_id = sample[1][0]
+        opcode_names = possible_opcodes(sample)
+        if opcode_id in opcodes:
+            opcodes[opcode_id] &= opcode_names
+        else:
+            opcodes[opcode_id] = opcode_names
+    work_done = True
+    while work_done:
+        definite = {k: v for k, v in opcodes.items() if len(v) == 1}
+        definite_names = set().union(*definite.values())
+        work_done = False
+        for k, v in opcodes.items():
+            if k not in definite:
+                old = len(v)
+                v -= definite_names
+                if len(v) < old:
+                    work_done = True
+    assert len(definite) == len(Instruction.ALL)
+    return {k: v.pop() for k, v in opcodes.items()}
 
 
 if __name__ == '__main__':
     lines = read_lines()
+    samples, program = extract_input(lines)
+
     part1 = 0
-    for sample in extract_samples(lines):
-        cnt = count_possible_opcodes(sample)
+    for sample in samples:
+        cnt = len(possible_opcodes(sample))
         if cnt >= 3:
             part1 += 1
     print("Part1: %s" % part1)
+
+    opcodes = find_opcodes(samples)
+    registers = [0, 0, 0, 0]
+    for opcode, a, b, c in program:
+        i = Instruction.ALL[opcodes[opcode]](a, b, c)
+        registers = i(registers)
+    print("Part2: register content is %s" % registers)
